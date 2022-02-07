a1f5c61927dbbe99984ac35abe7038a4db53dc66
brittany <brioden88@gmail.com>
2022-02-07 18:10:54 -0500

    key removal

--- src/services/GiphyApi.tsx
+++ src/services/GiphyApi.tsx
@@ -2,7 +2,7 @@ export function getTrending(){
 
     console.log()
 
-    return axios.get<>('https://api.giphy.com/v1/gifs/trending?api_key=grBlJrYKEILFojcVkBcw2uMMjtGY81TB&limit=25&rating=g')
+    return axios.get<>('https://api.giphy.com/v1/gifs/trending?api_key=')
                 .then(response => response.data)
 }
 
@@ -10,6 +10,6 @@ export function getSearched(){
 
     console.log()
 
-    return axios.get<>('https://api.giphy.com/v1/gifs/search?api_key=grBlJrYKEILFojcVkBcw2uMMjtGY81TB&q=&limit=25&offset=0&rating=g&lang=en')
+    return axios.get<>('https://api.giphy.com/v1/gifs/search?api_key')
                 .then(response => response.data)
 }
